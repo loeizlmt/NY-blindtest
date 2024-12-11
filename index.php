e0f1e55d9166fa7e2a27d579f3eb423ddf448161
loeizlmt <lolojardinets@outlook.fr>
2024-12-11 15:19:03 +0100

    detect click on buzzer

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <script>
+            function buzzed(){
+                var buzzé = true
+                if (buzzé === true) {
+                    alert("message");
+                    buzzé = false
+                }
+            }
+        </script>
+        <center><div id='buzzer' class="buzzer" onclick="buzzed()"></div></center> 
+    </body>
+</html>
+
+
